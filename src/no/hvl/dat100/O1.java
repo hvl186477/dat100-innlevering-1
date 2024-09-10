c98e84a7f09ca9c96395fa6e8b346e1d647edcb2
hvl186477 <186477@stud.hvl.no>
2024-09-10 20:56:00 +0200

    Update O1

--- src/no/hvl/dat100/O1.java
+++ src/no/hvl/dat100/O1.java
@@ -9,39 +9,41 @@ public class O1 {
 		String payroll = showInputDialog("Årslønn i antall hele kroner: ");
 
 		int pay = parseInt(payroll);
-		double trinnskatt;
-		
-		double trinnskatt1 = (292850-208051)*0.017;
-		double trinnskatt2 = (670000-292851)*0.040 + trinnskatt1;
-		double trinnskatt3 = (937900-671001)*0.136 + trinnskatt2;
-		double trinnskatt4 = (1350000-937901)*0.166 + trinnskatt3;
-				
+		double trinnskatt = 0;
+
+		double trinnskatt1 = (292850 - 208051) * 0.017;
+		double trinnskatt2 = (670000 - 292851) * 0.040 + trinnskatt1;
+		double trinnskatt3 = (937900 - 671001) * 0.136 + trinnskatt2;
+		double trinnskatt4 = (1350000 - 937901) * 0.166 + trinnskatt3;
+
 		String utTxt;
 
 		if (pay < 0) {
 			utTxt = "Ugyldig verdi";
 
-		} else if (pay >= 0 && pay <= 208050) {
-			utTxt = "Du betaler ingen trinnskatt";
-
+			// kalkulerer trinnskatt
 		} else if (pay >= 208051 && pay <= 292850) {
-			trinnskatt = (pay-208051)*0.017;
-			utTxt = "Du skal betale " + Math.round(trinnskatt) + " kr i trinnskatt";
+			trinnskatt = (pay - 208051) * 0.017;
 
 		} else if (pay >= 292851 && pay <= 670000) {
-			trinnskatt = (pay-292851)*0.04 + trinnskatt1;
-			utTxt = pay + " Du skal betale " + Math.round(trinnskatt) + " kr i trinnskatt";
+			trinnskatt = (pay - 292851) * 0.04 + trinnskatt1;
 
 		} else if (pay >= 671001 && pay <= 937900) {
-			trinnskatt = (pay-671001)*0.136 + trinnskatt2;
-			utTxt = "Du skal betale " + Math.round(trinnskatt) + " kr i trinnskatt";
+			trinnskatt = (pay - 671001) * 0.136 + trinnskatt2;
 
 		} else if (pay >= 937901 && pay <= 1350000) {
-			trinnskatt = (pay-937901)*0.166 + trinnskatt3;
-			utTxt = "Du skal betale " + Math.round(trinnskatt) + " kr i trinnskatt";
+			trinnskatt = (pay - 937901) * 0.166 + trinnskatt3;
+
+		} else if (pay > 1350000) {
+			trinnskatt = (pay - 1350001) * 0.176 + trinnskatt4;
+
+			// skriver ut hva du skal betale i trinnskatt
+		}
+
+		if (trinnskatt == 0) {
+			utTxt = "Du betaler ingen trinnskatt";
 
 		} else {
-			trinnskatt = (pay-1350001)*0.176 + trinnskatt4;	
 			utTxt = "Du skal betale " + Math.round(trinnskatt) + " kr i trinnskatt";
 
 		}
